fa5cad95f75b6241b0df2430db2816c42843f60a
KuzminaYuliya <kuzmina_h@mail.ru>
2021-06-25 21:31:57 +0500

    third commin for ajs_5_1_classes

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -4,31 +4,54 @@ import Magician from '../magician';
 import Swordsman from '../swordsman';
 import Undead from '../undead';
 import Zombie from '../zombie';
+import Character from '../character';
 
-test('should create players', () => {
+test('should create Bowerman player', () => {
   const bowermanPlayer = {
     name: 'Bowerman', type: 'Bowerman', health: 100, level: 1, attack: 25, defence: 25,
   };
+  expect(new Bowerman('Bowerman')).toEqual(bowermanPlayer);
+});
+
+test('should create Daemon player', () => {
   const daemonPlayer = {
     name: 'Daemon', type: 'Daemon', health: 100, level: 1, attack: 10, defence: 40,
   };
+  expect(new Daemon('Daemon')).toEqual(daemonPlayer);
+});
+
+test('should create Magician player', () => {
   const magicianPlayer = {
     name: 'Magician', type: 'Magician', health: 100, level: 1, attack: 10, defence: 40,
   };
+  expect(new Magician('Magician')).toEqual(magicianPlayer);
+});
+
+test('should create swordsmanPlayer player', () => {
   const swordsmanPlayer = {
     name: 'Swordsman', type: 'Swordsman', health: 100, level: 1, attack: 40, defence: 10,
   };
+  expect(new Swordsman('Swordsman')).toEqual(swordsmanPlayer);
+});
+
+test('should create undeadPlayer player', () => {
   const undeadPlayer = {
     name: 'Undead', type: 'Undead', health: 100, level: 1, attack: 25, defence: 25,
   };
+  expect(new Undead('Undead')).toEqual(undeadPlayer);
+});
+
+test('should create undeadPlayer player', () => {
   const zombiePlayer = {
     name: 'Zombie', type: 'Zombie', health: 100, level: 1, attack: 40, defence: 10,
   };
-  expect(new Bowerman('Bowerman')).toEqual(bowermanPlayer);
-  expect(new Bowerman('Bowerman9999999')).toEqual(new Error('Неверные данные'));
-  expect(new Daemon('Daemon')).toEqual(daemonPlayer);
-  expect(new Magician('Magician')).toEqual(magicianPlayer);
-  expect(new Swordsman('Swordsman')).toEqual(swordsmanPlayer);
-  expect(new Undead('Undead')).toEqual(undeadPlayer);
   expect(new Zombie('Zombie')).toEqual(zombiePlayer);
+});
+
+test('attempt to create player with wrong name', () => {
+  expect(() => new Bowerman('Bowerman9999999')).toThrow();
+});
+
+test('attempt to create player with wrong type', () => {
+  expect(() => new Character('Bowerman', 66)).toThrow();
 });
